7c063c51ccd80c152a1eaf2bce2049b1bee82ce7
taubenangriff <montag.lennard@gmail.com>
2022-08-27 20:47:06 +0200

    make example project runnable

---
+++ Dflat.Examples/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dflat.Examples
+{
+    public class Startup
+    {
+        public static void Main(String[] args)
+        {
+            CreateExamples.CreateChordWithRootAndIntervals();
+        }
+    }
+}
