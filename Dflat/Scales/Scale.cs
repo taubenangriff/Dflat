afb50cbfd66853c49557c169d30e1010ab4e3881
taubenangriff <montag.lennard@gmail.com>
2022-10-11 19:21:07 +0200

    Scales should be pitch sets

---
+++ Dflat/Scales/Scale.cs
@@ -0,0 +1,36 @@
+﻿using Dflat.Core;
+using Dflat.Intervals;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dflat.Scales
+{
+    public class Scale : IEnumerable<Pitch>
+    {
+        public Pitch BasePitch { get; }
+
+        public IEnumerable<Interval> ScaleDegrees { get => throw new NotImplementedException("Scale as Intervals not yet implemented"); private set; }
+        public IEnumerable<Pitch> Pitches { get; }
+
+        internal Scale(
+            Pitch basePitch,
+            IEnumerable<Pitch> pitches)
+        {
+            BasePitch = basePitch;
+            ScaleDegrees = Enumerable.Empty<Interval>();
+            Pitches = pitches;
+        }
+
+        public bool IsPartOfScale(Pitch p) => Pitches.Any(x => x.Equals(p));
+
+        public bool IsAnyEnharmonicEquivPartOfScale(Pitch p) => Pitches.Any(x => x.IsEnharmonicTo(p));
+
+        public IEnumerator<Pitch> GetEnumerator() => Pitches.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => Pitches.GetEnumerator();
+    }
+}
