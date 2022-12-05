f0ac34b9e6599edd600887ed98608e0163b18b8b
taubenangriff <montag.lennard@gmail.com>
2022-12-05 12:34:50 +0100

    Add GIS structures

---
+++ Dflat/GeneralizedIntervalSystems/IGeneralizedIntervalSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dflat.GeneralizedIntervalSystems
+{
+    public interface IGeneralizedIntervalSystem<TElement, TInterval> 
+        where TElement : IElement 
+        where TInterval : IInterval
+    {
+        TInterval GetInterval(TElement e1, TElement e2);
+
+        Type IntervalType => typeof(TInterval);
+        Type ElementType => typeof(TElement);
+    }
+}
---
+++ Dflat/GeneralizedIntervalSystems/IInterval.cs
@@ -0,0 +1,7 @@
+﻿namespace Dflat.GeneralizedIntervalSystems
+{
+    public  interface IInterval
+    {
+
+    }
+}
