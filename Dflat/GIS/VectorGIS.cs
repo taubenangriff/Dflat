c03a6102de2ae851940e3b33798c8165b5a46bd8
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-06-05 13:48:36 +0200

    Add an abstract Vector GIS

---
+++ Dflat/GIS/VectorGIS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dflat.GIS
+{
+    public class VectorGIS<T> : IGIS<Vector<T>, Vector<T>> where T : struct
+    {
+        public Vector<T> GetInterval(Vector<T> e1, Vector<T> e2) => e2 - e1;
+
+        public Vector<T> Transpose(Vector<T> element, Vector<T> interval) => element + interval; 
+    }
+}
