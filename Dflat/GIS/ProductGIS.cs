d3568419b8de4a976a7971aa45b136753d697bb2
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-06-05 13:48:14 +0200

    Add product GIS structures

---
+++ Dflat/GIS/ProductGIS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dflat.GIS
+{
+    public class ProductGIS<TE1, TE2, TI1, TI2> : IGIS<Tuple<TE1, TE2>, Tuple<TI1, TI2>>
+    {
+        IGIS<TE1, TI1> GIS1; 
+        IGIS<TE2, TI2> GIS2; 
+
+        public ProductGIS(
+            IGIS<TE1, TI1> gis1,
+            IGIS<TE2, TI2> gis2) 
+        {
+            GIS1 = gis1;
+            GIS2 = gis2; 
+        }
+        public Tuple<TI1, TI2> GetInterval(Tuple<TE1, TE2> e1, Tuple<TE1, TE2> e2)
+        {
+            return Tuple.Create(
+                GIS1.GetInterval(e1.Item1, e2.Item1), 
+                GIS2.GetInterval(e1.Item2, e2.Item2));
+        }
+
+        public Tuple<TE1, TE2> Transpose(Tuple<TE1, TE2> element, Tuple<TI1, TI2> interval)
+        {
+            return Tuple.Create(
+                GIS1.Transpose(element.Item1, interval.Item1),
+                GIS2.Transpose(element.Item2, interval.Item2)); 
+        }
+    }
+}
